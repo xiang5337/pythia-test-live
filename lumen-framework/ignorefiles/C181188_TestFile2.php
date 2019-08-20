97faacf75bf1215e8c8f86e3f817065e83682385
Stallion <xiang5337@hotmail.com>
2019-08-20 11:41:08 +0800

    Create C181188_TestFile2.php

---
+++ lumen-framework/ignorefiles/C181188_TestFile2.php
@@ -0,0 +1,55 @@
+## Ready State
+**Ready**
+
+## Summary
+- test
+## Ticket
+[1069300](https://portal.yopeso.com/task/1069300) 
+
+## Testing
+- no test
+
+
+
+
+
+
+
+
+1069300 
+
+- test
+
+Ticket: 1069300
+Reviewer: Stallion
+
+
+1069390-c181188-bug2
+1069390 C181188 BUG2## Ready State
+**Ready**
+
+## Summary
+- test
+## Ticket
+[1069300](https://portal.yopeso.com/task/1069300) 
+
+## Testing
+- no test
+
+
+
+
+
+
+
+
+1069300 
+
+- test
+
+Ticket: 1069300
+Reviewer: Stallion
+
+
+1069390-c181188-bug2
+1069390 C181188 BUG2
