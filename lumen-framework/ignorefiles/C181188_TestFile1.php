7d016dda112fabecc22d32a7e1e6dae3c19a72e3
Stallion <xiang5337@hotmail.com>
2019-08-20 11:38:56 +0800

    Create C181188_TestFile1.php

---
+++ lumen-framework/ignorefiles/C181188_TestFile1.php
@@ -0,0 +1,55 @@
+## Ready State
+**Ready**
+
+## Summary
+- test
+## Ticket
+[1069300](https://portal.yopeso.com/task/1069300) 
+
+## Testing
+- no test
+
+
+
+
+
+
+
+
+1069300 
+
+- test
+
+Ticket: 1069300
+Reviewer: Stallion
+
+
+1069389-c181188-bug1
+1069389 C181188 BUG1## Ready State
+**Ready**
+
+## Summary
+- test
+## Ticket
+[1069300](https://portal.yopeso.com/task/1069300) 
+
+## Testing
+- no test
+
+
+
+
+
+
+
+
+1069300 
+
+- test
+
+Ticket: 1069300
+Reviewer: Stallion
+
+
+1069389-c181188-bug1
+1069389 C181188 BUG1
