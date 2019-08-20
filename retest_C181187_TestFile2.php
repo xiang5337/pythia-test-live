dce2788488b53f27c8746e1a53a0d87616bf3ca7
Stallion <xiang5337@hotmail.com>
2019-08-20 11:06:29 +0800

    1069384 C181187 TASK2 retest (#45)
    1069384 
    
    - test
    
    Ticket: 1069384
    Reviewer: Stallion

---
+++ retest_C181187_TestFile2.php
@@ -0,0 +1,55 @@
+## Ready State
+**Ready**
+
+## Summary
+- test
+## Ticket
+[1069300](https://portal.yopeso.com/task/1069300) 
+
+## Testing
+- no test
+
+
+
+
+
+
+
+
+1069300 
+
+- test
+
+Ticket: 1069300
+Reviewer: Stallion
+
+
+1069384 C181187 TASK2 retest
+1069384-c181187-task2-retest## Ready State
+**Ready**
+
+## Summary
+- test
+## Ticket
+[1069300](https://portal.yopeso.com/task/1069300) 
+
+## Testing
+- no test
+
+
+
+
+
+
+
+
+1069300 
+
+- test
+
+Ticket: 1069300
+Reviewer: Stallion
+
+
+1069384 C181187 TASK2 retest
+1069384-c181187-task2-retest
